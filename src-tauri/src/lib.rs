8eda2d0ab48705ea230fdc1249c6840d12beac02
Jerome <charvet.jerome@gmail.com>
2026-03-04 06:54:54 +0000

    feat: add tauri-plugin-opener for opening external URLs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -166,6 +166,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_opener::init())
         .manage(AppState {
             timer,
             wallpaper: Mutex::new(WallpaperManager::new()),
